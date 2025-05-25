09360edc7e9adfaa8ed6c15d1e7273be12c2b288
kundanch240 <kundanch8181@gmail.com>
2025-05-25 09:52:39 +0530

    dashboard completed with css

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+
+// lodder start 
+const currentXP = 400;
+const maxXP = 500;
+const fillPercentage = (currentXP / maxXP) * 100;
+
+document.getElementById("xp-value").textContent = currentXP;
+document.getElementById("xp-bar-fill").style.width = fillPercentage + "%";
+
+// end
